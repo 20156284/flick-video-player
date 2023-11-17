6bebdec3ae00afc8245b352a161536c343eaa33b
will <willzh@live.cn>
2023-11-17 12:15:01 +0800

    add ios

--- lib/src/widgets/flick_native_video_player.dart
+++ lib/src/widgets/flick_native_video_player.dart
@@ -1,4 +1,3 @@
-import 'package:flick_video_player/src/utils/platform_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -17,7 +16,8 @@ class FlickNativeVideoPlayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformUtils.isMobile ? _buildMobile() : _buildDesktopOrWeb();
+    // return PlatformUtils.isMobile ? _buildMobile() : _buildDesktopOrWeb();
+    return _buildDesktopOrWeb();
   }
 
   Widget _buildMobile() {
